8d58b5b6f579aedc485a263108535e53328f82ab
Silver-Sword <38964705+Silver-Sword@users.noreply.github.com>
2021-09-01 15:56:03 -0400

    Add files via upload

---
+++ register.php
@@ -0,0 +1,41 @@
+<?php
+	$inData = getRequestInfo();
+
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$userId = $inData["userId"];
+	$Login = $inData["Login"];
+
+	$conn = new mysqli("localhost", "admin", "COP4331_7g", "COP4331");
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Users (UserId,FirstName,LastName,Login,Password) VALUES(?,?,?,?,?)");
+		$stmt->bind_param("sssss", $userId, $firstName, $lastName, $Login, $Password);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
