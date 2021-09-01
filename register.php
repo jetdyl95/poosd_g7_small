584be0b3f63d4e430643c6f53b5a54cf17a24256
Silver-Sword <38964705+Silver-Sword@users.noreply.github.com>
2021-09-01 16:20:17 -0400

    edited spaces

--- register.php
+++ register.php
@@ -6,7 +6,6 @@
 	$userId = $inData["userId"];	// check this later to make sure userId auto increments
 	$Login = $inData["Login"];
 	$Password = $inData["Password"];
-	// COMMENT
 
 	$conn = new mysqli("localhost", "admin", "COP4331_7g", "COP4331");
 	if ($conn->connect_error)
@@ -16,7 +15,7 @@
 	else
 	{
 		$Date = $conn->query(select now());
-		$stmt = $conn->prepare("INSERT into Users (UserId,DateCreated, DateLastLoggedIn, FirstName,LastName,Login,Password) VALUES(?,?,?,?,?,?,?)");
+		$stmt = $conn->prepare("INSERT into Users (UserId,DateCreated,DateLastLoggedIn,FirstName,LastName,Login,Password) VALUES(?,?,?,?,?,?,?)");
 		$stmt->bind_param("sssssss", $userId, $Date, $Date, $firstName, $lastName, $Login, $Password);
 		$stmt->execute();
 		$stmt->close();
