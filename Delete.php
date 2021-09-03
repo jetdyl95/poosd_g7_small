f5e06da4f6986820e0cc415f1d58a733fab21cf9
Dylan <jetdyl95@gmail.com>
2021-09-03 17:44:39 -0400

    added delete end point first draft

---
+++ Delete.php
@@ -0,0 +1,53 @@
+<?php
+  $inData = getRequestInfo();
+  $userId = $inData["userId"];
+  $contactId = $inData["contactId"];
+
+  $conn = new mysqli("localhost", "admin", "COP4331_7g", "COP4331");
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+
+  else
+  {
+    $checkContactExists = $conn->prepare("SELECT * FROM Contacts WHERE userId = ? AND contactId = ?");
+    $checkContactExists = $conn->bind_param("ss", $userId, $contactId);
+    $checkContactExists->execute();
+    $result = $checkContactExists->get_result();
+
+    if ($row = result->fetch_assoc())
+    {
+      $delQuery = $conn->prepare("SELECT * FROM Contacts WHERE userId = ? AND contactId = ?");
+      $delQuery = $conn->bind_param("ss", $userId, $contactId);
+      $delQuery->execute();
+      returnWithError("");
+    }
+
+    else
+    {
+      returnWithError("Could Not Find Contact. Deletion Failed.");
+    }
+
+    $conn->close();
+    $checkContactExists->close();
+    $delQuery->close();
+  }
+
+  function getRequestInfo()
+  {
+    return json_decode(file_get_contents('php://input'), true);
+  }
+
+  function sendResultInfoAsJson( $obj )
+  {
+    header('Content-type: application/json');
+    echo $obj;
+  }
+
+  function returnWithError( $err )
+  {
+    $retValue = '{"error":"' . $err . '"}';
+    sendResultInfoAsJson( $retValue );
+  }
+ ?>
