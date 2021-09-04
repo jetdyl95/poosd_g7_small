edcd8e3f635b867926a093ab2204a08f3760099b
Dylan <jetdyl95@gmail.com>
2021-09-04 00:12:54 -0400

    fixed prepared query logic syntax

--- Delete.php
+++ Delete.php
@@ -11,14 +11,14 @@
 
   else
   {
-    $checkContactExists = $conn->prepare("SELECT * FROM Contacts WHERE userId = ? AND contactId = ?");
+    $checkContactExists = $conn->prepare("SELECT FROM Contacts WHERE userId = ? AND contactId = ?");
     $checkContactExists = $conn->bind_param("ss", $userId, $contactId);
     $checkContactExists->execute();
     $result = $checkContactExists->get_result();
 
     if ($row = result->fetch_assoc())
     {
-      $delQuery = $conn->prepare("SELECT * FROM Contacts WHERE userId = ? AND contactId = ?");
+      $delQuery = $conn->prepare("DELETE FROM Contacts WHERE userId = ? AND contactId = ?");
       $delQuery = $conn->bind_param("ss", $userId, $contactId);
       $delQuery->execute();
       returnWithError("");
