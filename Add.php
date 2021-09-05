f8fc436408ee3fe7de7f0b748941c0d6d91f0119
Silver-Sword <38964705+Silver-Sword@users.noreply.github.com>
2021-09-05 15:16:49 -0400

    removed contactId from variables

--- Add.php
+++ Add.php
@@ -1,7 +1,6 @@
 <?php
   $inData = getRequestInfo();
   $fields = $inData["fields"];
-  $contactId = $inData["contactId"];
   $userId = $inData["userId"];
 
   $conn = new mysqli("localhost", "admin", "COP4331_7g", "COP4331");
@@ -11,8 +10,8 @@
 	}
   else
   {
-    $stmt = $conn->prepare("INSERT into Contacts (userId, contactId, firstName,lastName,email,phone) VALUES(?,?,?,?,?,?)");
-    $stmt->bind_param("ssss", $userId, $contactId, $firstName, $lastName, $email, $phone);
+    $stmt = $conn->prepare("INSERT into Contacts (userId, firstName,lastName,email,phone) VALUES(?,?,?,?,?)");
+    $stmt->bind_param("sssss", $userId, $firstName, $lastName, $email, $phone);
     $stmt->execute();
     $stmt->close();
     returnWithError("");
