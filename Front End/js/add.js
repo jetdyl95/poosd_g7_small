a9432c407ec2bb3fc952cdc2fb7ed018f99030f3
Chandler Hale <cbeckerhale@knights.ucf.edu>
2021-09-19 23:25:51 -0400

    Added feature where after adding a contact, erases all inputs in the text boxes

--- Front End/js/add.js
+++ Front End/js/add.js
@@ -2,36 +2,20 @@ var urlBase = 'http://COP4331-7.com/LAMPAPI';
 var extension = 'php';
 
 var userId = 0;
-<<<<<<< HEAD
-var firstName = "";
-var lastName = "";
-var phoneNum = "";
-var email = "";
-=======
 var FirstName = "";
 var LastName = "";
 var Phone = "";
 var Email = "";
->>>>>>> 1cc39556bbfcda0902d38de57c6dc5588812f56b
 
 
 function addContact()
 {
-<<<<<<< HEAD
-	var firstName = document.getElementById('FnameText').value;
-	var lastName = document.getElementById('LnameText').value;
-	var phoneNum = document.getElementById('PhoneNumText').value;
-	var email = document.getElementById('emailText').value;
-
-	var newContact = document.getElementById("contactText").value;
-=======
 //	var newContact = [];
 	var FirstName = document.getElementById('FnameText').value;
 	var LastName = document.getElementById('LnameText').value;
 	var Phone = document.getElementById('PhoneNumText').value;
 	var Email = document.getElementById('emailText').value;
 
->>>>>>> 1cc39556bbfcda0902d38de57c6dc5588812f56b
 	document.getElementById("contactAddResult").innerHTML = "";
 
 	var tmp = {fields:{FirstName:FirstName , LastName: LastName , Email: Email, Phone: Phone},UserID:userId};
@@ -60,5 +44,13 @@ function addContact()
 		document.getElementById("contactAddResult").innerHTML = err.message;
 	}
 
-}
+	finally
+	{
+        document.getElementById("FnameText").value = "";
+        document.getElementById("LnameText").value = "";
+        document.getElementById("PhoneNumText").value = "";
+        document.getElementById("emailText").value = "";
+  }
 
+
+}
