2243d665eebb9bbffd6aaddacb51aa93af0a5494
Cynthia Burke <89432645+smorbjorn@users.noreply.github.com>
2021-09-19 21:22:18 -0400

    Latest add.js

--- Front End/js/add.js
+++ Front End/js/add.js
@@ -1,32 +1,39 @@
 var urlBase = 'http://COP4331-7.com/LAMPAPI';
 var extension = 'php';
 
-// NOT FINISHED AND TESTED YET //
 var userId = 0;
-var firstName = "";
-var lastName = "";
+var FirstName = "";
+var LastName = "";
+var Phone = "";
+var Email = "";
 
 
 function addContact()
 {
-	var newContact = document.getElementById("contactText").value;
+//	var newContact = [];
+	var FirstName = document.getElementById('FnameText').value;
+	var LastName = document.getElementById('LnameText').value;
+	var Phone = document.getElementById('PhoneNumText').value;
+	var Email = document.getElementById('emailText').value;
+
 	document.getElementById("contactAddResult").innerHTML = "";
 
-	var tmp = {contact:newContact,userId,userId};
+	var tmp = {FirstName:FirstName , LastName: LastName , Email: Email, Phone: Phone,UserID:userId};
 	var jsonPayload = JSON.stringify( tmp );
 
-	var url = urlBase + '/Add.' + extension;
-	
+	var url = urlBase + '/AddTest.' + extension;
+
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 	try
 	{
-		xhr.onreadystatechange = function() 
+		xhr.onreadystatechange = function()
 		{
-			if (this.readyState == 4 && this.status == 200) 
+			if (this.readyState == 4 && this.status == 200)
 			{
 				document.getElementById("contactAddResult").innerHTML = "Contact has been added";
+				console.log( xhr.responseText);
 			}
 		};
 		xhr.send(jsonPayload);
@@ -35,5 +42,6 @@ function addContact()
 	{
 		document.getElementById("contactAddResult").innerHTML = err.message;
 	}
-	
+
 }
+
