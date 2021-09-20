46e67a31ecd7219f4eb87d1217a49fc3b4c95b76
Cynthia Burke <89432645+smorbjorn@users.noreply.github.com>
2021-09-19 21:19:22 -0400

    Latest Search

--- Front End/js/search.js
+++ Front End/js/search.js
@@ -1,5 +1,3 @@
-// NOT TESTED YET //
-
 var urlBase = 'http://COP4331-7.com/LAMPAPI';
 var extension = 'php';
 
@@ -7,48 +5,111 @@ var userId = 0;
 var firstName = "";
 var lastName = "";
 
+function confirmBox()
+{
+        var txt;
+        var r = confirm("Delete Contact?");
+        if (r == true) {
+                deleteContact();
+        } else {
+                txt = "pressed Cancel";
+        }
+        document.getElementById("deleteContactButton").innerHTML = txt;
+}
+
+
+function deleteContact()
+{
+        var contactId = document.getElementById("contactIdDel").value;
+        //var firstNameDel = document.getElementById("fNametoDel").value;
+        //var lastNameDel = document.getElementById("lNametoDel").value;
+
+        document.getElementById("contactDeleteResult").innerHTML = "";
+
+        var tmp = {userId:userId,contactId: contactId};
+        var jsonPayload = JSON.stringify( tmp );
+
+        var url = urlBase + '/Delete.' + extension;
+
+	var xhr = new XMLHttpRequest();
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+        try
+        {
+                xhr.onreadystatechange = function()
+                {
+                       if(this.readyState == 4 && this.status == 200)
+                        {
+                                var jsonObject = JSON.parse( xhr.responseText );
+                                document.getElementById("contactDeleteResult").innerHTML = "Contact has been deleted.";
+
+                        }
+                };
+                xhr.send(jsonPayload);
+        }
+        catch(err)
+        {
+                document.getElementById("contactDeleteResult").innerHTML = err.message;
+        }
+}
 
 function searchContact()
 {
-	var srch = document.getElementById("searchText").value;
-	document.getElementById("contactSearchResult").innerHTML = "";
-	
-	var contactList = "";
+        var srch = document.getElementById("searchText").value;
+        document.getElementById("contactSearchResult").innerHTML = "";
 
-	var tmp = {search:srch,userId:userId};
-	var jsonPayload = JSON.stringify( tmp );
+        var contactList = "";
 
-	var url = urlBase + '/Search.' + extension;
-	
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.onreadystatechange = function() 
-		{
-			if (this.readyState == 4 && this.status == 200) 
-			{
-				document.getElementById("contactSearchResult").innerHTML = "Contact(s) has been retrieved";
-				var jsonObject = JSON.parse( xhr.responseText );
-				
-				for( var i=0; i<jsonObject.results.length; i++ )
-				{
-					contactList += jsonObject.results[i];
-					if( i < jsonObject.results.length - 1 )
-					{
-						contactList += "<br />\r\n";
-					}
-				}
-				
-				document.getElementsByTagName("p")[0].innerHTML = contactList;
-			}
-		};
-		xhr.send(jsonPayload);
-	}
-	catch(err)
-	{
-		document.getElementById("contactSearchResult").innerHTML = err.message;
-	}
-	
-}
+        var tmp = {userId:userId, searchTerm:srch};
+        var jsonPayload = JSON.stringify( tmp );
+
+        var url = urlBase + '/Search.' + extension;
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+        try
+        {
+                xhr.onreadystatechange = function()
+                {
+                        if (this.readyState == 4 && this.status == 200)
+                        {
+                                document.getElementById("contactSearchResult").innerHTML = "Contact(s) has been retrieved";
+                                var jsonObject = JSON.parse( xhr.responseText );
+                                var results = jsonObject.results;
+                                //var temp = jsonObject.results.length;
+                                console.log(results);
+                                console.log("did my cats just ");
+
+                                for (const [key, val] of Object.entries(results))
+                                {
+
+                                        console.log("harmonize");
+                                        contactList += "Contact Id: ";
+                                        contactList += val.contactId;
+                                        contactList += " First Name: ";
+                                    contactList += val.firstName;
+                                        contactList += " Last Name: ";
+                                        contactList += val.lastName;
+                                        contactList += " Email: ";
+                                        contactList += val.email;
+                                        contactList += " Phone: ";
+                                        contactList += val.phone;
+                                        contactList += "<br />\r\n";
+					contactList += "<br />\r\n";
+                                }
+
+                                        console.log(contactList);
+
+                                        document.getElementsByTagName("p")[0].innerHTML = contactList;
+                        }
+                };
+                xhr.send(jsonPayload);
+        }
+         catch(err)
+        {
+                document.getElementById("contactSearchResult").innerHTML = err.message;
+        }
+ }
+
+
